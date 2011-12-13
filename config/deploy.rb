ece51edc0377c968fcce6317d9efca33d1ecab67
Timmy Crawford <timmydcrawford@gmail.com>
2011-12-12 16:12:56 -0800

    cap changes

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,13 @@ role :web, "freshies"                          # Your HTTP server, Apache/etc
 role :app, "freshies"                          # This may be the same as your `Web` server
 role :db,  "freshies", :primary => true # This is where Rails migrations will run
 set :deploy_to, '/home/timmy/conepatrol.com'
-after :deploy, "symlink_assets"
+
+desc "Symlink db config and public assets"    
+task :symlink_assets, :roles => :app, :except => {:no_release => true, :no_symlink => true} do
+  run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/"
+  run "ln -nfs #{shared_path}/config/environments/production.rb #{release_path}/config/environments/"
+  run "ln -nfs #{shared_path}/tmp/ #{release_path}/tmp/"
+end
 
 namespace :deploy do
   task :start do ; end
@@ -16,11 +22,5 @@ namespace :deploy do
   task :restart, :roles => :app, :except => { :no_release => true } do
     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
   end
-
-  desc "Symlink db config and public assets"    
-  task :symlink_assets, :roles => :app, :except => {:no_release => true, :no_symlink => true} do
-    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/"
-    run "ln -nfs #{shared_path}/config/environments/production.rb #{release_path}/config/environments/"
-    run "ln -nfs #{shared_path}/tmp/ #{release_path}/tmp/"
-  end
 end
+after "deploy", "symlink_assets"

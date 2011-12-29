0534724a197ea956bdca7d42103c1d368d761f91
Timmy Crawford <timmydcrawford@gmail.com>
2011-12-29 09:47:46 -0800

    tunes

--- app/controllers/alerts_controller.rb
+++ app/controllers/alerts_controller.rb
@@ -3,7 +3,9 @@ class AlertsController < ApplicationController
     @alert = Alert.find(params[:id])
     verb = Twilio::Verb.new { |v|
       if @alert
-      v.say "Yo bro, this is the cone patrol powder alert robot.  Bachelor is reporting #{@alert.snow_report.snowfall_twelve} inches of snow overnight.  It is currently #{@alert.snow_report.base_temp} degrees at the base. Go shred buddy!"
+      v.play 'http://conepatrol.com/kids.mp3'
+      v.pause '2'
+      v.say "Yo bro, this is the cone patrol powder alert robot.  Bachelor is reporting #{@alert.snow_report.snowfall_twelve} inches of snow overnight.  It is currently #{@alert.snow_report.base_temp} degrees at the base. Go shred buddy!", :voice => 'woman'
       end
       v.hangup
     }

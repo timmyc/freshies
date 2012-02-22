132ff352dae1f7d994b893f4d8d9594d8e46b32f
Timmy Crawford <timmydcrawford@gmail.com>
2012-02-22 13:10:15 -0800

    fix for hoodoo

--- lib/hoodoo.rb
+++ lib/hoodoo.rb
@@ -11,23 +11,21 @@ class Hoodoo
   end
 
   def get_data
-    updated = self.doc.at_xpath('//li[@class="sidebox "]//div[@class="textwidget"]//p//b//font').text
+    updated = self.doc.at_xpath('//div[@id="right"]//li[@class="sidebox "]//div[@class="textwidget"]//strong').text
     table_data = {}
     report_table_cells = self.doc.xpath('//li[@class="sidebox "]//div[@class="textwidget"]//table//tr//td')
+    report_table_cells = self.doc.xpath('//table[@class="easy-table-creator"]//tr//td')
     report_table_cells.each_with_index do |td,i|
-      if i.even?
-        key = slug_me(strip_junk(td.text()))
-      else
-        key = slug_me(strip_junk(report_table_cells[i-1].text()))
-      end
-      table_data[key] = strip_junk(td.text())
+      next unless td.children && td.children.size >= 2
+      key = slug_me(strip_junk(td.children.first.text()))
+      table_data[key] = strip_junk(td.children[1].text())
     end
     #{:lifts_open=>" ed, manzanita, easy rider", :runs_open=>"75%", :new_snow=>"1\"", :autobahn_tubing=>"closed m - th", :snow_depth=>"20.8\"", :snow_bunny_sled_hill=>"closed", :weather=>"snowing", :temp_lodge=>"32", :temp_peak=>"26"}
     self.readings = {
       :report_time => Time.zone.parse(updated),
-      :snowfall_twelve => table_data[:"new_snow:_12_hours"].to_i,
-      :base_temp => table_data[:templodge].to_i,
-      :base_depth => table_data[:depth].to_i
+      :snowfall_twelve => table_data[:new_snow_24_hrs].to_i,
+      :base_temp => table_data[:temp_lodge].to_i,
+      :base_depth => table_data[:snow_level].to_i
     }
   end
 
@@ -37,7 +35,7 @@ class Hoodoo
 
   def slug_me(str)
     str = str.downcase.strip
-    str = str.gsub(/\.|\(|\)/,'')
+    str = str.gsub(/\:|\.|\(|\)/,'')
     str = str.gsub(' ','_')
     return str.to_sym
   end

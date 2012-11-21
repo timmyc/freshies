6f774c0e8ba45a4c9add8a3785cb1e7dbfa356bf
Timmy Crawford <timmydcrawford@gmail.com>
2012-11-20 21:15:32 -0800

    ios handler

--- app/controllers/api/ios_controller.rb
+++ app/controllers/api/ios_controller.rb
@@ -1,6 +1,6 @@
 class Api::IosController < ApplicationController
 
-  def index
+  def create
     #@shredder = Shredder.find_or_create_from_android(params)
     #@shredder.android_subscriptions.first.update_from_app(params)
     render :json => {:success => true, :opts => params}
